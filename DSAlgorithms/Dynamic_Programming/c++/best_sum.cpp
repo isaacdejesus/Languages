5fa98579c2714e809bca2780287e5abee744c15a
isaacdejesus <isaacderxc@gmail.com>
2023-01-24 23:54:02 -0600

    finished best sum

--- DSAlgorithms/Dynamic_Programming/c++/best_sum.cpp
+++ DSAlgorithms/Dynamic_Programming/c++/best_sum.cpp
@@ -2,12 +2,12 @@
 #include <vector>
 #include <unordered_map>
 
-//std::vector<int> best_sum(int target, std::vector<int>, std::unordered_map<int, int>);
+std::vector<int> best_sum_memoized(int target, std::vector<int>, std::unordered_map<int, std::vector<int>>);
 std::vector<int> best_sum(int , std::vector<int>);
 int main()
 {
     std::vector<int> v1 = {3, 5, 2};
-    std::vector<int> result = best_sum(8, v1);
+    std::vector<int> result = best_sum_memoized(8, v1);
     for(auto item : result)
         std::cout<<item<<' ';
     std::cout<<'\n';
@@ -39,4 +39,34 @@ std::vector<int> best_sum(int target_sum, std::vector<int> numbers)
     }
     
     return shortest_combination;
+}
+
+std::vector<int> best_sum_memoized(int target_sum, std::vector<int> numbers, std::unordered_map<int, std::vector<int>> cache)
+{
+    if(cache.count(target_sum))
+        return cache[target_sum];
+    if(target_sum == 0)
+        return {};
+    if(target_sum < 0)
+        return {-1};
+    
+    std::vector<int> shortest_combination;
+    for(int i = 0; i < numbers.size(); i++)
+    {
+        int remainder = target_sum - numbers[i];
+        std::vector<int> remainder_combination = best_sum_memoized(remainder, numbers, cache);
+        if(remainder_combination.empty())
+        {
+            remainder_combination.push_back(numbers[i]);
+            shortest_combination = remainder_combination;
+        }
+        if(!remainder_combination.empty() && remainder_combination[0] != -1)
+        {
+            remainder_combination.push_back(numbers[i]);
+            if(shortest_combination.empty() || remainder_combination.size() < shortest_combination.size())
+                shortest_combination = remainder_combination;
+        }
+    }
+    cache[target_sum] = shortest_combination;
+    return cache[target_sum];
 }
