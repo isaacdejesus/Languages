21f613c89176baf75a916afa92864cca98a25a2f
isaacdejesus <isaacderxc@gmail.com>
2023-02-10 19:28:52 -0600

    iosijdf

---
+++ DSAlgorithms/Leetcode/c++/148-sort_list.cpp
@@ -0,0 +1,52 @@
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+class Solution {
+public:
+    ListNode* sortList(ListNode* head) {
+       if(head == nullptr || head->next == nullptr) 
+            return head;
+        ListNode* mid = get_mid(head);
+        ListNode* left = sortList(head);
+        ListNode* right = sortList(mid);
+        return merge(left, right);
+    }
+
+    ListNode* merge(ListNode* list1, ListNode* list2)
+    {
+        ListNode* new_list = new ListNode();
+        ListNode* current = new_list;
+        while(list1 != nullptr && list2 != nullptr)
+        {
+            if(list1->val < list2->val)
+            {
+                current->next = list1;
+                list1 = list1->next;
+            }
+            else
+            {
+                current->next = list2;
+                list2 = list2->next;
+            }
+            current = current->next;
+        }
+        current->next = (list1 != nullptr) ? list1 : list2;
+        return new_list->next;
+    }
+    ListNode* get_mid(ListNode* head)
+    {
+        ListNode* mid_prev = nullptr;
+        while(head != nullptr && head->next != nullptr)
+        {
+            mid_prev = (mid_prev == nullptr) ? head : mid_prev->next;
+            head = head->next->next;
+        }
+        ListNode* mid = mid_prev->next;
+        mid_prev->next = nullptr;
+        return mid;
+    }
+};
---
+++ DSAlgorithms/Leetcode/c++/92-reverse_linked_list_2.cpp
@@ -0,0 +1,44 @@
+
+ struct ListNode {
+     int val;
+     ListNode *next;
+     ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ };
+ 
+class Solution {
+public:
+    ListNode* reverseBetween(ListNode* head, int left, int right) {
+       if(left == right) 
+            return head;
+        ListNode* current = head;
+        ListNode* prev = nullptr;
+        for(int i = 0; current != nullptr && i < left - 1; i++)
+        {
+            prev = current;
+            current = current->next;
+        }
+        ListNode* last = prev;
+        ListNode* new_end = current;
+        //reverse between left and right
+        ListNode* next = current->next;
+        for(int i = 0; current!= nullptr && i < right - left + 1; i++)
+        {
+            current->next = prev;
+            prev = current;
+            current = next;
+            if(next != nullptr)
+                next = next->next;
+        }
+        if(last != nullptr)
+            last->next = prev;
+        else
+            head = prev;
+        
+        new_end->next = current;
+        return head;
+
+        
+    }
+};
