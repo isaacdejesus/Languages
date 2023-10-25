7483dd376978974901e302723bf077d51babd5f9
isaacdejesus <isaacderxc@gmail.com>
2023-10-25 17:07:51 -0500

    updated ll code

--- DSAlgorithms/Leetcode/c++/linked_list/147-insertion_sort.cpp
+++ DSAlgorithms/Leetcode/c++/linked_list/147-insertion_sort.cpp
@@ -13,22 +13,22 @@ public:
     ListNode* insertionSortList(ListNode* head) {
         if(head == nullptr || head->next == nullptr) 
             return head;
-        ListNode* dum = new ListNode(0, head);
-        ListNode* current = head->next;
-        ListNode* prev = head;
-        ListNode* temp;
+        ListNode* dum = new ListNode(0, head);  //node to always point to head. Used to start looping from head
+        ListNode* current = head->next; //start w 2nd node bc 1st node is already sorted
+        ListNode* prev = head; //points to first node
+        ListNode* temp; //used to loop from head to find insert loc for current node
         while(current != nullptr)
         {
-            if(current->val >= prev->val)
+            if(current->val >= prev->val) //advance as long as current >= prev
             {
                 prev = current;
                 current = current->next;
                 continue;
             }
-            temp = dum;
-            while(current->val > temp->next->val)
+            temp = dum;  //set temp to head
+            while(current->val > temp->next->val) //loop to find insert loc for current
                 temp = temp->next;
-            prev->next =  current->next;
+            prev->next =  current->next;  //insert current at temp loc
             current->next = temp->next;
             temp->next = current;
             current = prev->next;
--- DSAlgorithms/Leetcode/c++/linked_list/25-reverse_nodes_in_k_groups.cpp
+++ DSAlgorithms/Leetcode/c++/linked_list/25-reverse_nodes_in_k_groups.cpp
@@ -11,7 +11,7 @@
  class Solution {
 public:
     ListNode* reverseKGroup(ListNode* head, int k) {
-       if(k <= 1 || head == nullptr) 
+       if(k <= 1 || head == nullptr)  //if only 1 node, no need to reverse
             return head;
        ListNode* current = head;
        ListNode* prev = nullptr;
@@ -19,19 +19,18 @@ public:
        {
             ListNode* last = prev;
             ListNode* new_end = current;
-            //reverse between left and right
             ListNode* next = current->next;
             ListNode* temp = nullptr;
             int count = 0;
-            temp = current;
-            while(temp != nullptr ) 
+            temp = current;  //pointer to current node
+            while(temp != nullptr )  //count node to the right of current
             {
                 temp = temp->next;
                 count++;  
             }           
-            if(count < k)
+            if(count < k)  //if < k nodes, means there are less than k nodes left on list. Done
                 break;
-            for(int i = 0; current!= nullptr && i < k ; i++)
+            for(int i = 0; current!= nullptr && i < k ; i++)  //reverse k nodes
             {
                 current->next = prev;
                 prev = current;
@@ -39,15 +38,15 @@ public:
                 if(next != nullptr)
                     next = next->next;
             }
-            if(last != nullptr)
+            if(last != nullptr)  //if nodes to the left, link left side to the newly reversed head
                 last->next = prev;
             else
-                head = prev;
+                head = prev;  //if no nodes to the left, newly reversed head is the new head
         
-            new_end->next = current;
-            if(current == nullptr)
+            new_end->next = current;  //link end of newly reversed list to next element on list/to the right
+            if(current == nullptr)  //if current is null, reached end of list. Done
                 break; 
-            prev = new_end;
+            prev = new_end; //update prev to be end of newly reversed list
        }
         return head;
     }
