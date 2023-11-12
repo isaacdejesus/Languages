af2a3cc76702cc42b7b1fd8b09eee76cf03c183b
isaac <idreyes@uh.edu>
2023-11-12 17:14:23 -0600

    added heaps

---
+++ DSAlgorithms/Trees/java/heaps/Main.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+class Main{
+    public static void main(String[] args)throws Exception
+    {
+        Heap<Integer> heap = new Heap<>();
+        heap.insert(34);
+        heap.insert(45);
+        heap.insert(22);
+        heap.insert(89);
+        heap.insert(76);
+        //System.out.println(heap.remove());
+        ArrayList list = heap.heapSort();
+        System.out.println(list);
+
+    }
+}
