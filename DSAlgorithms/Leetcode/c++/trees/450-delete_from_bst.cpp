c29d42fbd1c1b0d6a84d5aee6379c29c7b8e896f
isaac <idreyes@uh.edu>
2023-12-13 04:04:36 -0600

    fixed treees

--- DSAlgorithms/Leetcode/c++/trees/450-delete_from_bst.cpp
+++ DSAlgorithms/Leetcode/c++/trees/450-delete_from_bst.cpp
@@ -1,19 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
 class Solution {
 public:
     TreeNode* deleteNode(TreeNode* root, int key) {
-        if(root) 
-            if(key < root->val) root->left = deleteNode(root->left, key);     //We frecursively call the function until we find the target node
-            else if(key > root->val) root->right = deleteNode(root->right, key);       
-            else{
-                if(!root->left && !root->right) return NULL;          //No child condition
-                if (!root->left || !root->right)
-                    return root->left ? root->left : root->right;    //One child contion -> replace the node with it's child
-					                                                //Two child condition   
-                TreeNode* temp = root->left;                        //(or) TreeNode *temp = root->right;
-                while(temp->right != NULL) temp = temp->right;     //      while(temp->left != NULL) temp = temp->left;
-                root->val = temp->val;                            //       root->val = temp->val;
-                root->left = deleteNode(root->left, temp->val);  //        root->right = deleteNode(root->right, temp);		
-            }
+        if(root == nullptr)
+            return root;
+        if(key > root->val) 
+            root->right = deleteNode(root->right, key);
+        else if(key < root->val)
+            root->left = deleteNode(root->left, key);
+        else  //found node to be deleted
+        {
+            if (root->left == nullptr)  //if no children, will replace node with nullptr
+                return root->right;
+            else if(root->right == nullptr)  //if one child, will replace node with this child
+                return root->left;
+            //node has 2 children, find min val in right sub tree
+            TreeNode* current = root->right;
+            while(current->left != nullptr)
+                current = current->left;
+            root->val = current->val;  //replace node with min val in right subtree
+            root->right = deleteNode(root->right, root->val);  //rec right subtree to delete duplicate val
+        }
         return root;
-    }   
+    }
 };
